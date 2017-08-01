ac6153b82072b3aeea91adcfad50e8042b6cc2dc
Joyabali Kadiwal <joyabalikadiwal@yahoo.com>
2017-08-01 12:20:53 -0500

    added canvas stuff

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,44 @@ angular.module('myApp.view1', ['ngRoute','ui.bootstrap'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope','$log', '$interval','$http','canvasService',
 
-}]);
+	function($scope, $log, $interval,$http,canvasService) {
+
+		   $scope.X = 10;
+           $scope.Y = 100;
+
+
+          var coordinates = $interval(function() {
+          	var canvas = document.getElementById("myCanvas");
+	        var ctx = canvas.getContext("2d");
+	        ctx.transform(1, 0, 0, -1, 0, canvas.height)
+	        ctx.moveTo(-10,-10);
+	        ctx.moveTo(200,100);
+
+          	var promise = canvasService.getCoordinates(100);
+            promise.then(
+            function(payload) { 
+                console.log(JSON.stringify(payload.data));
+                $scope.X = payload.data.X;
+                $scope.Y = payload.data.Y;
+                },
+            function(errorPayload) {
+              $log.error('failure loading user', errorPayload);
+            });
+
+
+
+	        ctx.fillRect($scope.X,$scope.Y,5,5);
+
+          }, 5000);
+
+}])
+
+.factory('canvasService', function($http) {
+    return {
+      getCoordinates: function(id) {
+         return $http.get('https://test-gse00012255.apaas.us6.oraclecloud.com/person/' + id);
+      }
+    }
+  });
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,27 +1,33 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute','ui.bootstrap'])
+angular.module('myApp.view2',['ngRoute','ui.bootstrap'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider','$httpProvider', function($routeProvider,$httpProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
     controller: 'View2Ctrl'
   });
+
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
 }])
 
-.controller('View2Ctrl', ['$scope', '$uibModal', '$log',
+.controller('View2Ctrl', ['$scope', '$uibModal', '$log','$http','userService',
 
-
-	function($scope, $uibModal, $log) {
-        $scope.showForm = function () {
-            $scope.message = "Show Form Button Clicked";
-            console.log($scope.message);
-	    	
-            $scope.user = {
-	    	id:'1',
-        	name: 'Bob',
-        	description: 'I am awesome'
-    		};
+	function($scope, $uibModal, $log, $http,userService) {
+        $scope.status;
+        $scope.showForm = function (id) {
+            var promise = userService.getEmployee(id);
+            promise.then(
+            function(payload) { 
+                console.log(JSON.stringify(payload.data));
+                $scope.user = payload.data;
+                },
+            function(errorPayload) {
+              $log.error('failure loading user', errorPayload);
+            });
 
             var modalInstance = $uibModal.open({
                 templateUrl: 'components/modal-form.html',
@@ -44,12 +50,37 @@ angular.module('myApp.view2', ['ngRoute','ui.bootstrap'])
             });
         };
 
-}]);
+}])
 
-var ModalInstanceCtrl = function ($scope, $uibModalInstance, userForm, user) {
+ .factory('userService', function($http) {
+    return {
+      getEmployee: function(id) {
+         return $http.get('https://test-gse00012255.apaas.us6.oraclecloud.com/person/' + id);
+      },
+
+      updateEmployee: function(user){
+        user.photo = 'null';
+        var data = {data:user};
+        console.log(JSON.stringify(data));
+        return $http.put('https://test-gse00012255.apaas.us6.oraclecloud.com/person/' + user.id, data);
+      }
+
+    }
+  });
+
+var ModalInstanceCtrl = function ($scope, $uibModalInstance, userForm, user, userService) {
     $scope.submitForm = function () {
         if ($scope.form.userForm.$valid) {
-          console.log(JSON.stringify($scope.user.name));
+            
+            var promise = userService.updateEmployee($scope.user);
+            promise.then(
+            function(payload) { 
+                $scope.status = 'Updated Customer! Refreshing customer list.';
+                },
+            function(errorPayload) {
+                $scope.status = 'Unable to update customer: ' + errorPayload.message;
+            });
+            
             $uibModalInstance.close('closed');
         } else {
             console.log('form is not in scope');
